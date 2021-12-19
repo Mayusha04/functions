08f4f2da965d8de4c5a1111bb32cc73c40ebe50e
Mayusha04 <mayushachowdary95@gmail.com>
2021-12-19 23:57:07 +0530

    hi

---
+++ functions.js
@@ -0,0 +1,27 @@
+function nameOfFunction (){
+
+}  
+console.log(typeof nameOfFunction);
+
+//
+function greet(name){  
+     console.log("hii" +" " + name + " "+ "everyone");
+
+}
+
+greet("CGCS");
+
+//
+
+function add(a,b){
+        return a + b;
+}
+var result=add(15,20);
+console.log("the addition of two numbers is" +" "+result);
+
+//
+function sub(c,d){
+    return c-d;
+}
+var output=sub(7,2);
+console.log("the substraction of 7 and 2 is:" +" "+output);
